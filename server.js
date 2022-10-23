e3557803892f227cab9ad7b6fc53c39c4f85d24e
Wendy Wilgus <wilguswendy@gmail.com>
2022-10-23 17:59:57 -0400

    working on server.js

--- server.js
+++ server.js
@@ -1,25 +1,38 @@
 
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const consoleTable = require('console.table');
+const util = require('util');
+const { ADDRGETNETWORKPARAMS } = require("dns");
+require('dotenv').config();
 
 const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'MVUEdAMS02<0',
+        password: process.env.pw,
         database: 'tracker_db'
     },
 );
 
-function mainPrompt() {
+const query = util.promisify(db.query).bind(db);
+
+const mainPrompt = () => {
     console.log("Welcome to your Employee Tracker.");
     inquirer
         .prompt([
             {
                 type: "list",
                 message: "What would you like to do?",
-                choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role"],
-                name: "department",
+                choices: [
+                    "View All Departments", 
+                    "View All Roles", 
+                    "View All Employees", 
+                    "Add a Department", 
+                    "Add a Role", 
+                    "Add an Employee", 
+                    "Update an Employee Role"],
+                name: "mainlist",
             },
         ])
 }
@@ -30,43 +43,89 @@ function viewRoles
     select all of the roles from a table
 function viewEmployees
     select all of the employees from a table  
-function addDepartment ()   {
+
+const addDepartment = () =>  {
     inquirer
         .prompt([
             {
                 type: "input",
                 message: "What is the name of the department?",
                 name: "department",
+                validate: (input) => {
+                    if (input.trim() === "") {
+                        return false;
+                    }
+                    return true;
+                }
             }
         ])//then add to database
+        .then(ans => {
+            db.query(`INSERT INTO department (name) VALUES (?)`, ans.name, function (err, results) {
+                mainPrompt();
+            });
+        });
 };
-function addRole () {
+
+//add a role to Role table
+async function addRole()  {
+    //array for department names created by user
+    const departments = [];
+
+    const departmentEntry = [];
+    await query(`SELECT id, name FROM department`).then(res => {
+        res.forEach(department => {
+            departments.push(department.name)
+            departmentEntry.push(department);
+        });
+    });
     inquirer
         .prompt([
+            {
+                type: "list",
+                message: "What is the department for this role?",
+                name: "department",
+                choices: departments,
+            },
             {
                 type: "input",
                 message: "What is the employee's role?",
                 name: "title",
-            },
-            {
-                type: "input",
-                message: "What is the employee's name?",
-                name: "name",
+                validate: (input) => {
+                    if (input.trim() === ""){
+                        return false;
+                    }
+                    return true;
+                }
             },
             {
                 type: "input",
                 message: "What is the employee's salary?",
                 name: "salary",
-
+                validate: (input) => {
+                    if (isNaN(input)){
+                        return "Please enter a number for the salary."
+                    }
+                    return true;
+                }
             },
-            {
-                type: "input",
-                message: "What is the department for this role?",
-                name: "department",
-            }
+            
         ]) //add to database
+        .then(async ans => {
+            let department_id;
+            departmentEntry.forEach(department => {
+                if(department.name === ans.department){
+                    department_id = parseInt(department.id);
+                }
+            });
+            let salary = parseInt(ans.salary);
+            await query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`, [ans.title, salary, department_id]
+                .then(res => console.log(`Added ${ans.title} to roles.`)));
+            mainPrompt();
+        });
 };
-function addEmployee () {
+
+//function to add a new employee to Employee table
+async function addEmployee () {
     inquirer
         .prompt([
             {
